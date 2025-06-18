449c3dbe214f6d5929848e1d0ca58066f25661df
RochelManatsoa <48514290+RochelManatsoa@users.noreply.github.com>
2025-06-18 18:08:32 +0300

    Add employee interface for white label

---
+++ src/WhiteLabel/Controller/Client1/EmployeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\WhiteLabel\Controller\Client1;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_EMPLOYE')]
+#[Route('/employe')]
+class EmployeController extends AbstractController
+{
+    public function __construct(
+        private ManagerRegistry $managerRegistry,
+        private EntityManagerInterface $entityManager,
+    ) {
+        $this->entityManager = $managerRegistry->getManager('client1');
+    }
+
+    #[Route('/', name: 'app_white_label_client1_employe')]
+    public function index(): Response
+    {
+        return $this->render('white_label/client1/employe/index.html.twig');
+    }
+
+    #[Route('/mes-infos', name: 'app_white_label_client1_employe_profile')]
+    public function profile(): Response
+    {
+        return $this->render('white_label/client1/employe/profile.html.twig');
+    }
+}
