fa30a17c257d7debdcb6d4159e6b4db8b28e9e9c
Nirina <nirinarocheldev@gmail.com>
2025-06-30 15:58:45 +0200

    joblisting voter

---
+++ src/WhiteLabel/Security/Client1/Voter/JobListingVoter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\WhiteLabel\Security\Client1\Voter;
+
+use App\WhiteLabel\Entity\Client1\Entreprise\JobListing;
+use App\WhiteLabel\Entity\Client1\User;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class JobListingVoter extends Voter
+{
+    public const EDIT = 'JOB_LISTING_EDIT';
+    public const VIEW = 'JOB_LISTING_VIEW';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return in_array($attribute, [self::EDIT, self::VIEW])
+            && $subject instanceof JobListing;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        if(!$subject instanceof JobListing){
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::EDIT:
+                if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
+                    return true;
+                }
+                return $subject->getEntreprise()->getId() === $user->getEntrepriseProfile()->getId();
+                break;
+
+            case self::VIEW:
+                return true;
+                break;
+        }
+
+        return false;
+    }
+}
